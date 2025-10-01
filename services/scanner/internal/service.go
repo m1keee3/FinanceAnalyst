fcd7cfa089293b985fed7d0e0b2c1d2625057c8e
m1keee3 <skirdamisa@gmail.com>
2025-10-01 11:25:54 +0300

    feat(services/scanner): add StatsCompute interface

---
+++ services/scanner/internal/service.go
@@ -0,0 +1,9 @@
+package internal
+
+import (
+	"github.com/m1keee3/FinanceAnalyst/common/models"
+)
+
+type StatsComputer interface {
+	ComputeStats(matches []models.ChartSegment, daysToWatch int) (*models.ScanStats, error)
+}
