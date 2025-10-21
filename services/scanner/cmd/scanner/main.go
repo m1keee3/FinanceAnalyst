7c7c97761d1eb4e1ae58719218dfc88e47e1fadb
m1keee3 <skirdamisa@gmail.com>
2025-10-21 19:38:53 +0000

    feat(services/scanner): feat main

---
+++ services/scanner/cmd/scanner/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/m1keee3/FinanceAnalyst/pkg/logger/handlers/slogpretty"
+	"github.com/m1keee3/FinanceAnalyst/services/scanner/internal/app"
+	"github.com/m1keee3/FinanceAnalyst/services/scanner/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	application := app.New(log, cfg.Grpc.Port)
+
+	application.GRPCServer.MustRun()
+
+	// TODO db application
+
+	// TODO grpc сервис
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
