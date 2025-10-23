45f8deb35e9c29ea21d4f80ee1ca8837e91738eb
m1keee3 <skirdamisa@gmail.com>
2025-10-23 11:16:02 +0000

    feat(services/scanner): implement ToProto func

--- services/scanner/internal/mapper/chart_segment.go
+++ services/scanner/internal/mapper/chart_segment.go
@@ -3,6 +3,7 @@ package mapper
 import (
 	"github.com/m1keee3/FinanceAnalyst/services/scanner/domain/models"
 	scannerv1 "github.com/m1keee3/FinanceAnalyst/services/scanner/proto-gen/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func FromProtoChartSegment(proto *scannerv1.ChartSegment) models.ChartSegment {
@@ -23,6 +24,28 @@ func FromProtoChartSegment(proto *scannerv1.ChartSegment) models.ChartSegment {
 		Ticker:  proto.Ticker,
 		From:    proto.GetFrom().AsTime(),
 		To:      proto.GetTo().AsTime(),
+		Candles: candles,
+	}
+}
+
+func ToProtoChartSegment(segment models.ChartSegment) *scannerv1.ChartSegment {
+
+	candles := make([]*scannerv1.Candle, len(segment.Candles))
+
+	for i, c := range segment.Candles {
+		candles[i] = &scannerv1.Candle{
+			Date:  timestamppb.New(c.Date),
+			Open:  c.Open,
+			High:  c.High,
+			Low:   c.Low,
+			Close: c.Close,
+		}
+	}
+
+	return &scannerv1.ChartSegment{
+		Ticker:  segment.Ticker,
+		From:    timestamppb.New(segment.From),
+		To:      timestamppb.New(segment.To),
 		Candles: candles,
 	}
 }
